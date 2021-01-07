51355954d3240538a25a4a8d2a8b206450eb5eac
Juan López Rubio <j.lopez.r@gmail.com>
2021-01-07 22:01:28 +0100

    Star moves

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody2D starBody;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        starBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
+    }
+}

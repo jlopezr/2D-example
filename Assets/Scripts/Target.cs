a5b2d5013748421b82eb95845e17cff08b052c62
Juan López Rubio <j.lopez.r@gmail.com>
2021-01-08 22:13:50 +0100

    Target hits

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // When target is hit
+        if (col.gameObject.tag == "Bullet")
+        {
+            Debug.Log("Target was Hit!");
+        }
+    }
+
+}

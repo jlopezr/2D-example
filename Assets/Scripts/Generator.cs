8767f97e57ee0c982c1fb28391621738260f412d
Juan López Rubio <j.lopez.r@gmail.com>
2021-01-08 20:37:11 +0100

    Space instantiates falling diamond

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject boxToGenerate;
+
+    void Update()
+    {
+        // When spacebar is hit
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // instantiate the box object
+            Instantiate(boxToGenerate);
+        }
+    }
+
+}

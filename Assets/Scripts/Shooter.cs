c1ab84525385f6eb4768e375ac40f6f25a067938
Juan López Rubio <j.lopez.r@gmail.com>
2021-01-08 21:25:34 +0100

    Shooter fires bullets

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject fireball;
+    public Rigidbody2D body;
+    public float speed;
+
+    void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
+
+        // When spacebar is hit
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // instantiate the fireball object            
+            Instantiate(fireball,
+                new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0),
+                new Quaternion(0, 0, 0, 0)
+            );
+        }
+    }
+
+}

bd95fb88c04aab9724f4a2097945dd25ce2a012d
Juan López Rubio <j.lopez.r@gmail.com>
2021-01-08 21:38:18 +0100

    Split movement and shooting

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,13 +5,9 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     public GameObject fireball;
-    public Rigidbody2D body;
-    public float speed;
 
     void Update()
-    {
-        body.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
-
+    {        
         // When spacebar is hit
         if (Input.GetKeyDown(KeyCode.Space))
         {

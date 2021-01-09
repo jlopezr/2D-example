a71ca34e8f9edeb2e2252189f4e7e3fc7e30f36d
Juan López Rubio <j.lopez.r@gmail.com>
2021-01-09 07:04:45 +0100

    Merge two png to form a new sprite

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,11 @@ public class Movement : MonoBehaviour
 
     public Rigidbody2D starBody;
     public float speed;
+    private bool sombrero = false;
+
+    public Texture2D bottom;
+    public Texture2D top;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +25,24 @@ public class Movement : MonoBehaviour
         var v = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
         Debug.DrawLine(starBody.position, starBody.position + v, Color.red);
         starBody.velocity = v * speed;
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (!sombrero)
+            {
+                sombrero = true;
+
+                Texture2D combined = bottom.AlphaBlend(top);
+
+                Sprite sprite = Sprite.Create(
+                    combined,
+                    new Rect(0, 0, combined.width, combined.height),
+                    new Vector2(0.5f, 0.5f)
+                );
+
+                gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
+
+            }
+        }
     }
 }
